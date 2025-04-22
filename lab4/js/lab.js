5910d0efe930a4a67b154e3c0e1af68c7e5b868f
LiamEngstromCoding <Li11iaEternity@gmail.com>
2025-04-21 18:57:26 -0700

    Lab 4
    Commenting list in .js file and added lists and psuedocoding in the index.html

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,16 +1,25 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Liam Engstrom
+// Date: April 21st, 2025
 
-// Constants
+//Program Name: Rock-Paper-Scissors
+    //Assign players to either designations “Player 1” and “Player 2”
 
-// Functions
+    //When round starts, provide 3 available the inputs (rock, paper, scissors) for the first player
+          //Prompt them to select an input
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+    //Once input is given by “Player 1”, Provide the inputs again and prompt the second player to choose an input.
+    
+    //After both players have selected inputs, decide if a player has reached a winning instance. 
+          //Winning instances: 
+              //Rock beats scissors
+              //Scissors beats paper
+              //Paper beats rock
+    //If both players play the same input, the round is a draw, and no one gets points. The round starts over
+    
+    //The player that has reached the winning instance gains a point
+        //First player to reach two points wins the game
+
 
 function main() {
   console.log("Main function started.");
